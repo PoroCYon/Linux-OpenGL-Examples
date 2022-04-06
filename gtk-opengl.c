53631804908ad31c4f6b479388a7f88c7db92ab0
PoroCYon <porocyon@titandemo.org>
2022-04-06 23:40:33 +0200

    GTK: exit on pressing escape; easier way to animate; more comments

--- gtk-opengl.c
+++ gtk-opengl.c
@@ -23,13 +23,27 @@ static char* vshader = "#version 450\nvec2 y=vec2(1.,-1);vec4 x[4]={y.yyxx,y.xyx
 GLuint vao;
 GLuint p;
 
-static gboolean
-on_render (GtkGLArea *glarea, GdkGLContext *context)
+// if you want to exit on escape
+static gboolean on_esc(GtkWidget* wnd, GdkEventKey* ev)
 {
+	if (ev->keyval == GDK_KEY_Escape) gtk_main_quit();
+	// alternatively, call exit_group(2) directly, as shown at the very end
+
+	return FALSE;
+}
+
+static gboolean on_render(GtkGLArea *glarea, GdkGLContext *context)
+{
+	// draw our fullscreen shader
 	glUseProgram(p);
 	glBindVertexArray(vao);
 	glVertexAttrib1f(0, 0);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	// needed if you want animations, don't call this if you are making an exegfx
+	gtk_gl_area_queue_render(glarea);
+	// idk if this is needed, but it doesn't hurt
+	while (gtk_events_pending()) gtk_main_iteration();
 
 	return TRUE;
 }
@@ -110,11 +124,17 @@ static void on_realize(GtkGLArea *glarea)
 
 	// // Start updating:
 	// gdk_frame_clock_begin_updating(frame_clock);
+
+	// ---------
+	// nah, the above is all lies, just do this (plus one extra call in on_render):
+	gtk_gl_area_queue_render(glarea);
 }
 
 void _start() {
-	asm volatile("sub $8, %rsp\n");
+	asm volatile("sub $8, %rsp\n"); // align the stack -- may not be needed with smol
 
+	// call gtk_init, but saving some space by ignoring the second parameter
+	// (of which the value doesn't matter anyway)
 	typedef void (*voidWithOneParam)(int*);
 	voidWithOneParam gtk_init_one_param = (voidWithOneParam)gtk_init;
 	(*gtk_init_one_param)(NULL);
@@ -127,15 +147,22 @@ void _start() {
 	g_signal_connect(glarea, "realize", G_CALLBACK(on_realize), NULL);
 	g_signal_connect(glarea, "render", G_CALLBACK(on_render), NULL);
 
+	// if you want exit on escape:
+	g_signal_connect(win, "key_press_event", G_CALLBACK(on_esc), NULL);
+
 	gtk_widget_show_all (win);
 
+	// this makes the window go fullscreen
 	gtk_window_fullscreen((GtkWindow*)win);
+
+	// this hides the cursor
 	GdkWindow* window = gtk_widget_get_window(win);
 	GdkCursor* Cursor = gdk_cursor_new(GDK_BLANK_CURSOR);
 	gdk_window_set_cursor(window, Cursor);
 
 	gtk_main();
 
+	// call exit_group(2), this exits the process
 	asm volatile(".intel_syntax noprefix");
 	asm volatile("push 231"); //exit_group
 	asm volatile("pop rax");
